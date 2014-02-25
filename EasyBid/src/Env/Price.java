7e38339cb9f0bccd15e0d15b286a676ca00614e8
EzioGalante <galante@et.esiea.fr>
2014-02-25 17:14:57 +0100

    Implémentation de la base de price

--- EasyBid/src/Env/Price.java
+++ EasyBid/src/Env/Price.java
@@ -1,5 +1,36 @@
 package Env;
 
+
 public class Price {
 
+	private final String current="";
+	private int value;
+	
+	public enum currency {
+		DOLLARD ("$"),
+		LIVRE ("£"),
+		YEN ("´"),
+		EURO ("Û");
+	
+		private currency(String current){
+	        this.current = current;
+	    }
+
+
+	    public String getValue(){return current;}
+
+	}
+	
+	
+	
+	
+
+	
+	public void setValue(int value) {
+		this.value=value;
+		
+	}
+	
+	
+	
 }
