fc7e5f38933c7edd9c58e981cfc8f77308b7a6c4
kinesine <matthias.leclair@gmail.com>
2014-03-10 20:14:31 +0100

    Dans ProductTest, simplification du code

--- EasyBid/test/environment/ProductTests.java
+++ EasyBid/test/environment/ProductTests.java
@@ -8,45 +8,28 @@ import user.User;
 
 public class ProductTests {
 	private AuctionHall testHall = new AuctionHall();
-	
-	
+	User i = new User("Product", "Owner", new Price(20, Currency.EURO), this.testHall);
+	Product testproduct = new Product(i, new Price(750, Currency.EURO), "Coffe Table");
 	
 	@Test
 	public void testProduct() {
-		
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetName() {
-		User i = new User("Product", "Owner", new Price(20, Currency.EURO), this.testHall);
-		Product testproduct = new Product(i, new Price(750, Currency.EURO), "Coffe Table");
-		//this.testHall.addUser(i);
 		assertEquals("Coffe Table", testproduct.getName());
-				
-		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetCurrentPrice() {
-		User i = new User("Product", "Owner", new Price(20, Currency.EURO), this.testHall);
-		Product testproduct = new Product(i, new Price(750, Currency.EURO), "Coffe Table");
-		//this.testHall.addUser(i);
 		assertEquals(750, testproduct.getCurrentPrice().getValue(),0);
 		assertEquals(Currency.EURO, testproduct.getCurrentPrice().getCurrency());
-		
-		
-		
-		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testGetOwner() {
-		User i = new User("Product", "Owner", new Price(20, Currency.EURO), this.testHall);
-		Product testproduct = new Product(i, new Price(750, Currency.EURO), "Coffe Table");
-		
 		assertEquals(i, testproduct.getOwner());
-		//fail("Not yet implemented");
 	}
 
 	@Test
