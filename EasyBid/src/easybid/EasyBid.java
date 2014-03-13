fa2267c0b4109f1e0043587d91ad749af7ee6555
kinesine <matthias.leclair@gmail.com>
2014-03-13 18:33:10 +0100

    implementation de la classe EasyBid

---
+++ EasyBid/src/easybid/EasyBid.java
@@ -0,0 +1,37 @@
+package easybid;
+
+import user.User;
+import environment.AuctionHall;
+import environment.Currency;
+import environment.Price;
+
+public class EasyBid {
+
+	private AuctionHall hall = new AuctionHall();
+	
+	public EasyBid(){
+		
+		
+	/*
+		User user = null;
+		User user1 = null;
+		
+		try{
+			user = new  User("Pierre","Hewins", new Price(50, Currency.EURO), hall);
+			user1 = new  User("Ezio","Galante",new Price(50, Currency.EURO), hall);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			return;
+		}
+		hall.addUser(user);
+		hall.addUser(user1);
+		
+		System.out.println("HALL : "+hall);
+		System.out.println("Users in the hall :");
+		for(User i : hall.getKnownUsers())
+			System.out.println(i.toString());
+		//System.out.println(maliste);
+		 * */
+		 
+	}
+}
